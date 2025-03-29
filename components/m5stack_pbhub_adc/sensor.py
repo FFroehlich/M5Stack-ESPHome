0bc36aab9aa15d5acf87d1b8415a66bb2c2b2a3d
Fabian Fröhlich <fabian@schaluck.com>
2025-03-29 07:31:11 +0000

    Update sensor.py

--- components/m5stack_pbhub_adc/sensor.py
+++ components/m5stack_pbhub_adc/sensor.py
@@ -23,13 +23,12 @@ from esphome.const import (
     UNIT_VOLT,
 )
 
-from esphome.components.m5stack_pbhub import mstack_pbhub_ns
+from esphome.components.m5stack_pbhub import mstack_pbhub_ns, M5StackPBHUBComponent
 
 CONF_M5STACK_PBHUB_ID = "m5stack_pbhub_id"
 CONF_CHANNEL = "channel"
 CONF_PIN = "pin"
 
-PbHub = mstack_pbhub_ns.class_("PortHub", cg.Component)
 PbHubADCSensor = mstack_pbhub_ns.class_("PbHubADCSensor", sensor.Sensor, cg.PollingComponent)
 
 def validate_config(config):
@@ -48,7 +47,7 @@ CONFIG_SCHEMA = CONFIG_SCHEMA = cv.All(
         {
             cv.Required(CONF_PIN): adc.validate_adc_pin,
             cv.GenerateID(): cv.declare_id(PbHubADCSensor),
-            cv.Required(CONF_M5STACK_PBHUB_ID): cv.use_id(PbHub),
+            cv.Required(CONF_M5STACK_PBHUB_ID): cv.use_id(M5StackPBHUBComponent),
             cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=5),
             cv.Required(CONF_PIN): cv.int_range(min=0, max=1),
             cv.Optional(CONF_RAW, default=False): cv.boolean,
