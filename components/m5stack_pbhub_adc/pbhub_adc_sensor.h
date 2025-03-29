96703befb2a547c6f57445774ed4e45e5b4fd6cd
Fabian Fröhlich <fabian@schaluck.com>
2025-03-29 07:40:28 +0000

    Update pbhub_adc_sensor.h

--- components/m5stack_pbhub_adc/pbhub_adc_sensor.h
+++ components/m5stack_pbhub_adc/pbhub_adc_sensor.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "porthub.h"
+#include "esphome/components/m5stack_pbhub/porthub.h"
 
 namespace esphome {
 namespace m5stack_pbhub {
