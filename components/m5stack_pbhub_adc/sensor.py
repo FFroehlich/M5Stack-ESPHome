6566d0d0e0df601655cbae43dbcb33158bf47fe9
Fabian Fröhlich <fabian@schaluck.com>
2025-03-29 07:06:12 +0000

    Update sensor.py

--- components/m5stack_pbhub_adc/sensor.py
+++ components/m5stack_pbhub_adc/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import adc
+from esphome.components import adc, sensor
 from esphome.const import (
     CONF_ID,
     CONF_UNIT_OF_MEASUREMENT,
