2d11c7180c952fa233d0ff5e3a6c9c857bc31fda
Fabian Fröhlich <fabian@schaluck.com>
2025-03-29 07:47:21 +0000

    Update pbhub_adc_sensor.h

--- components/m5stack_pbhub_adc/pbhub_adc_sensor.h
+++ components/m5stack_pbhub_adc/pbhub_adc_sensor.h
@@ -3,11 +3,12 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/m5stack_pbhub/porthub.h"
+#include "esphome/components/adc/adc.h"
 
 namespace esphome {
 namespace m5stack_pbhub {
 
-class PbHubADCSensor : public sensor::Sensor, public PollingComponent {
+class PbHubADCSensor : public adc::ADCSensor, public PollingComponent {
  public:
   PbHubADCSensor(PortHub *parent, uint8_t channel, uint8_t pin)
       : parent_(parent), channel_(channel), pin_(pin) {}
@@ -15,8 +16,7 @@ class PbHubADCSensor : public sensor::Sensor, public PollingComponent {
   void update() override {
     // Compute register address for the given channel/pin
     uint8_t reg = channel_ * 10 + pin_;  // Example encoding
-    uint16_t raw = parent_->hub_a_read_value(reg);
-    float voltage = (raw / 1023.0f) * 5.0f;  // scale to 0–5V
+    uint16_t raw = parent_->hub_a_sample(reg);
     this->publish_state(voltage);
   }
 
